bb52e6e6fa3a2a88a3d5be866ce4896401922096
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-05-25 05:16:45 +0800

    add Swagger configuration for API documentation

---
+++ src/config/swagger.js
@@ -0,0 +1,36 @@
+const schema = require('../swagger/shema.swagger');
+const path = require('path');
+
+const swaggerConfig = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'Ramal Kripto',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8000',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+      schemas: {
+        components: schema.components,
+      },
+    },
+  },
+  apis: [
+    path.resolve(__dirname, '../routes/*.js'),
+    path.resolve(__dirname, '../swagger/*.js'),
+  ],
+};
+
+module.exports = swaggerConfig;
