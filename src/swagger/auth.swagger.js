d40ccaeffd80c75286bb881b0ace6f0f268be3d4
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-05-25 06:15:21 +0800

    add Swagger documentation for authentication endpoints

---
+++ src/swagger/auth.swagger.js
@@ -0,0 +1,147 @@
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     summary: Signin
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: mochrul.kurniawan@gmail.com
+ *                 required: true
+ *               password:
+ *                 type: string
+ *                 example: User_123
+ *                 required: true
+ *     responses:
+ *       200:
+ *         description: Login Successfully!
+ *       404:
+ *         description: Login Failed
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     summary: Register
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: Mochammad Syahrul Kurniawan
+ *                 required: true
+ *               email:
+ *                 type: string
+ *                 example: mochrul.kurniawan@gmail.com
+ *                 required: true
+ *               password:
+ *                 type: string
+ *                 example: User_123
+ *                 required: true
+ *     responses:
+ *       200:
+ *         description: Successful registration!
+ *       400:
+ *         description: Email already exists
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ * /auth/verify-email/{token}:
+ *   get:
+ *     tags:
+ *       - Authentication
+ *     summary: Verify account
+ *     parameters:
+ *       - name: token
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Verify Successfully
+ *       404:
+ *         description: Verify Failed
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ * /auth/request-reset-password:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     summary: Request reset password
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: mochrul.kurniawan@gmail.com
+ *                 required: true
+ *     responses:
+ *       200:
+ *         description: Reset password request sent successfully
+ *       400:
+ *         description: Invalid email address
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ * /auth/reset-password/{token}:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     summary: Reset password
+ *     parameters:
+ *       - name: token
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               password:
+ *                 type: string
+ *                 example: "User_123"
+ *                 required: true
+ *     responses:
+ *       200:
+ *         description: Reset password successfully
+ *       400:
+ *         description: Invalid token or password
+ *       500:
+ *         description: Internal Server Error
+ */
