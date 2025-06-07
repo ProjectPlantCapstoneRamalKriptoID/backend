2ba55703abc0556f0a90ea1852225cdc384cfb17
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-06-07 16:31:59 +0800

    refactor: clean up user.controller.js by removing unused imports and comments

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,4 @@
 const User = require('../models/user.model');
-// const path = require('path');
-// const fs = require('fs').promises;
-
-// require('dotenv').config();
 
 // Get profile
 exports.getProfile = async (req, res) => {
@@ -13,6 +9,37 @@ exports.getProfile = async (req, res) => {
     }
 
     res.status(200).json({ data: user });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error', err });
+  }
+};
+
+// Update profile
+exports.updateProfile = async (req, res) => {
+  const { name, email } = req.body || {};
+
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    // Update name & email jika disediakan
+    if (name) user.name = name;
+    if (email) user.email = email;
+
+    // Update profileImages dari Cloudinary
+    if (req.files?.profileImages) {
+      user.profileImages = req.files.profileImages.map((file) => ({
+        url: file.path,
+        filename: file.filename,
+        public_id: file.public_id || file.filename,
+      }));
+    }
+
+    await user.save();
+    res.status(200).json({
+      message: 'Profile updated successfully',
+      data: user,
+    });
   } catch (err) {
     res.status(500).json({ message: 'Internal Server Error', err });
   }
