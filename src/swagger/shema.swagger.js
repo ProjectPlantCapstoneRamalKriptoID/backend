959e185b492e931a66cb5dc9b521d76660cd98a3
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-06-07 15:04:31 +0800

    refactor: update Swagger schemas for User and Verification components

--- src/swagger/shema.swagger.js
+++ src/swagger/shema.swagger.js
@@ -2,28 +2,13 @@
  * @swagger
  * components:
  *   schemas:
- *     Authentication:
- *       type: object
- *       required:
- *         - email
- *         - password
- *       properties:
- *         email:
- *           type: string
- *         password:
- *           type: string
- */
-
-/**
- * @swagger
- * components:
- *   schemas:
  *     User:
  *       type: object
  *       required:
  *         - name
  *         - email
  *         - password
+ *         - verified
  *       properties:
  *         id:
  *           type: string
@@ -35,6 +20,32 @@
  *           type: string
  *         verified:
  *           type: boolean
+ *         createdAt:
+ *           type: date-time
+ *         updatedAt:
+ *           type: date-time
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Verification:
+ *       type: object
+ *       required:
+ *         - id
+ *         - userId
+ *         - uniqueString
+ *         - expiredAt
+ *       properties:
+ *         id:
+ *           type: string
+ *         userId:
+ *           type: string
+ *         uniqueString:
+ *           type: string
+ *         expiredAt:
+ *           type: date-time
  *         createdAt:
  *           type: date-time
  *         updatedAt:
