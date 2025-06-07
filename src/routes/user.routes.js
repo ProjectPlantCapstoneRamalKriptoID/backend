7de9508341fa35b16761b721aa5d3e44c424ef8c
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-06-07 16:32:18 +0800

    feat: implement profile update route with Cloudinary image upload

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,18 +2,22 @@ module.exports = (app) => {
   const router = require('express').Router();
   const user = require('../controllers/user.controller');
   const { authentication } = require('../middleware/authentication.middleware');
+  const { upload } = require('../configs/cloudinary.config');
 
   // Get user profile
   router.get('/profile', authentication(), (req, res) => {
     user.getProfile(req, res);
   });
 
-  //   // Update user profile
-  //   router.put(
-  //     '/profile',
-  //     authenticationMiddleware,
-  //     userController.updateProfile
-  //   );
+  // Update user profile
+  router.put(
+    '/profile-update',
+    authentication(),
+    upload.fields([{ name: 'profileImages', maxCount: 1 }]),
+    (req, res) => {
+      user.updateProfile(req, res);
+    }
+  );
 
   //   // Delete user account
   //   router.delete(
