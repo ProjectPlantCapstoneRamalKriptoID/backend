0f9a9ab6e55645d44a3f3c72dece742a04b2eebe
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-06-08 05:13:49 +0800

    feat: add deletePhotoProfile function to handle profile image deletion

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model');
+const cloudinary = require('cloudinary').v2;
 
 // Get profile
 exports.getProfile = async (req, res) => {
@@ -38,6 +39,34 @@ exports.updateProfile = async (req, res) => {
     await user.save();
     res.status(200).json({
       message: 'Profile updated successfully',
+      data: user,
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error', err });
+  }
+};
+
+// Delete photo profile
+exports.deletePhotoProfile = async (req, res) => {
+  const public_id = req.query.public_id;
+
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    // Delete in cloudinary
+    await cloudinary.uploader.destroy(public_id);
+
+    // Delete in database
+    const profileImages = user.profileImages.filter(
+      (image) => image.public_id !== public_id
+    );
+
+    user.profileImages = profileImages;
+    await user.save();
+
+    res.status(200).json({
+      message: 'Profile image deleted successfully',
       data: user,
     });
   } catch (err) {
